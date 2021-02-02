700df4254307aea2814e972b1c4fb7a723ac3fda
snow915 <snow915@protonmail.com>
2021-02-01 23:32:02 -0600

    add: first express route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
+app.get("/getRestaurants", (request, response) => {
+   console.log("Get all restaurants");
+   response.json({
+      status: "success",
+      restaurant: "Carls JR"
+   });
+});
+
 app.listen(process.env.PORT, () => {
    console.log(`Server is up and listening on port ${process.env.PORT}`);
 });
