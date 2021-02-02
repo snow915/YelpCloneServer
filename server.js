a717c7bc9e4671f5f6594289562a6f06eae4c98a
snow915 <snow915@protonmail.com>
2021-02-01 23:54:53 -0600

    add: more routes for get restaurants and post 1 restaurant

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.get("/getRestaurants", (request, response) => {
+// GET ALL RESTAURANTS
+app.get("/api/v1/restaurants", (request, response) => {
    console.log("Get all restaurants");
    response.json({
       status: "success",
       restaurant: "Carls JR"
    });
+});
+
+//GET A RESTAURANT
+app.get("/api/v1/restaurants/:id", (request, response) => {
+   console.log(request.params);
+});
+
+//CREATE A RESTAURANT
+app.post("/api/v1/restaurants", (request, response) => {
+   console.log(request);
 });
 
 app.listen(process.env.PORT, () => {
