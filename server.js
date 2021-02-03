a1fee6690661d31afe0f0282b1fd01a00a66d683
snow915 <snow915@protonmail.com>
2021-02-02 20:09:19 -0600

    add: code for create, update and delete content in db

--- server.js
+++ server.js
@@ -40,33 +40,58 @@ app.get("/api/v1/restaurants/:id", async (request, response) => {
 });
 
 //CREATE A RESTAURANT
-app.post("/api/v1/restaurants", (request, response) => {
-   console.log(request.body);
-   response.status(201).json({
-      status: "success",
-      data: {
-         restaurant: "some restaurant"
-      }
-   });
+app.post("/api/v1/restaurants", async (request, response) => {
+   try {
+      let body = request.body;
+      const data = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *", [body.name, body.location, body.price_range]);
+      response.status(201).json({
+         status: "success",
+         data: {
+            restaurant: data.rows
+         }
+      });
+   } catch (e) {
+      console.log(e);
+   }
+
 });
 
 //UPDATE RESTAURANT
-app.put("/api/v1/restaurants/:id", (request, response) => {
-   console.log(request.params.id);
-   console.log(request.body);
-   response.status(200).json({
-      status: "success",
-      data: {
-         restaurant: "some restaurant"
-      }
-   });
+app.put("/api/v1/restaurants/:id", async (request, response) => {
+   try{
+      let id = request.params.id;
+      let body = request.body;
+      const data = await db.query("UPDATE restaurants SET name=$1, location=$2, price_range=$3 WHERE id = $4 RETURNING *", [body.name, body.location, body.price_range, id]);
+      response.status(200).json({
+         status: "success",
+         data: {
+            restaurant: data.rows
+         }
+      });
+   } catch (e) {
+      console.log(e);
+   }
+
 });
 
 //DELETE RESTAURANT
-app.delete("/api/v1/restaurants/:id", (request, response) => {
-   response.status(204).json({
-      status: "success"
-   });
+app.delete("/api/v1/restaurants/:id", async (request, response) => {
+   try{
+      const row = await db.query("DELETE FROM restaurants WHERE id=$1", [request.params.id]);
+      if(row.rowCount > 0){
+         response.status(204).json({
+            status: "deleted"
+         });
+      } else {
+         response.status(204).json({
+            status: "no_deleted"
+         });
+      }
+
+   } catch (e) {
+      console.log(e);
+   }
+
 });
 
 app.listen(process.env.PORT, () => {
